a2b53f1a588ff41a41f521d6f70bd963903ebe5b
zhen-zen <66577170+zhen-zen@users.noreply.github.com>
2020-08-16 18:57:09 -0700

    Basic refactoring for VoodooI2CSensorHubEnabler::enableHub based on previous implementation; more information required.

--- VoodooI2CHID/Sensors/VoodooI2CSensorHubEnabler.cpp
+++ VoodooI2CHID/Sensors/VoodooI2CSensorHubEnabler.cpp
@@ -24,52 +24,42 @@ bool VoodooI2CSensorHubEnabler::start(IOService* parent) {
 
     IOLog("%s::%s Found Intel ACPI Sensor Hub Enabler\n", getName(), name);
     
-    if (enableHub() != kIOReturnSuccess)
+    UInt32 result;
+    if (enableHub(&result) != kIOReturnSuccess || !result)
         return false;
-    
+
+    IOLog("%s::%s Enabled Sensor Hub\n", getName(), name);
+
     registerService();
     
     return true;
 }
 
-IOReturn VoodooI2CSensorHubEnabler::enableHub() {
-    UInt32 guid_1 = 0x03C868D5;
-    UInt32 guid_2 = 0x563F42A8;
-    UInt32 guid_3 = 0x9F579A18;
-    UInt32 guid_4 = 0xD949B7CB;
-    
-    OSObject *result = NULL;
-    OSObject *params[4];
-    char buffer[16];
-    
-    memcpy(buffer, &guid_1, 4);
-    memcpy(buffer + 4, &guid_2, 4);
-    memcpy(buffer + 8, &guid_3, 4);
-    memcpy(buffer + 12, &guid_4, 4);
-    
-    
-    params[0] = OSData::withBytes(buffer, 16);
-    params[1] = OSNumber::withNumber(0x1, 8);
-    params[2] = OSNumber::withNumber(0x1, 8);
-    params[3] = OSNumber::withNumber((unsigned long long)0x0, 8);
-    
-    provider->evaluateObject("_DSM", &result, params, 4);
-    if (!result)
-        provider->evaluateObject("XDSM", &result, params, 4);
-    if (!result) {
-        IOLog("%s::%s Could not find suitable _DSM or XDSM method in ACPI tables\n", getName(), name);
-        return kIOReturnNotFound;
-    }
-    
-    IOLog("%s::%s Enabled Sensor Hub\n", getName(), name);
-    
-    if (result)
-        result->release();
-    
+IOReturn VoodooI2CSensorHubEnabler::enableHub(UInt32 *result) {
+    IOReturn ret;
+    uuid_t guid;
+    uuid_parse(SHAD_DSM_GUID, guid);
+
+    // convert to mixed-endian
+    *(reinterpret_cast<uint32_t *>(guid)) = OSSwapInt32(*(reinterpret_cast<uint32_t *>(guid)));
+    *(reinterpret_cast<uint16_t *>(guid) + 2) = OSSwapInt16(*(reinterpret_cast<uint16_t *>(guid) + 2));
+    *(reinterpret_cast<uint16_t *>(guid) + 3) = OSSwapInt16(*(reinterpret_cast<uint16_t *>(guid) + 3));
+
+    OSObject *params[] = {
+        OSData::withBytes(guid, 16),
+        OSNumber::withNumber(SHAD_DSM_REVISION, 8),
+        OSNumber::withNumber(SHAD_ON_INDEX, 8),
+        OSArray::withCapacity(1),
+    };
+
+    ret = provider->evaluateInteger("_DSM", result, params, 4);
+    if (ret != kIOReturnSuccess)
+        ret = provider->evaluateInteger("XDSM", result, params, 4);
+
     params[0]->release();
     params[1]->release();
     params[2]->release();
     params[3]->release();
-    
-    return kIOReturnSuccess;
+
+    return ret;
 }
--- VoodooI2CHID/Sensors/VoodooI2CSensorHubEnabler.hpp
+++ VoodooI2CHID/Sensors/VoodooI2CSensorHubEnabler.hpp
@@ -17,6 +17,10 @@
 
 #include "../../../../Dependencies/helpers.hpp"
 
+#define SHAD_DSM_GUID "03c868d5-563f-42a8-9f57-9a18d949b7cb"
+#define SHAD_DSM_REVISION 1
+#define SHAD_ON_INDEX 1
+
 class EXPORT VoodooI2CSensorHubEnabler : public IOService {
   OSDeclareDefaultStructors(VoodooI2CSensorHubEnabler);
 
@@ -27,7 +31,7 @@ class EXPORT VoodooI2CSensorHubEnabler : public IOService {
     const char* name;
     IOACPIPlatformDevice* provider;
     
-    IOReturn enableHub();
+    IOReturn enableHub(UInt32 *result);
 };
 
 
